6d84fd7705f915490a9aab53a5ee4fe1884dade5
Felix Rieseberg <fr@makenotion.com>
2024-10-18 08:27:05 -0700

    feat: Don't load native module if WS_NO_BUFFER_UTIL is set

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 try {
+  if (process.env.WS_NO_BUFFER_UTIL) {
+    throw new Error('WS_NO_BUFFER_UTIL is set');
+  }
+  
   module.exports = require('node-gyp-build')(__dirname);
 } catch (e) {
   module.exports = require('./fallback');
